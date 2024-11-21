3b6977982f3c3cdc1bbbb4f3418523161a52ef47
Luis Henrique <42474086+henriqueannicchino@users.noreply.github.com>
2024-11-21 17:50:24 +0100

    feat: added cards and columns

---
+++ src/App.js
@@ -0,0 +1,130 @@
+function App() {
+  return (
+    <div className="trello-master">
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://img.acp.pt/ResourcesUser/ImagesAC/Classicos/2022/Mes06/Ferrari-F40-35-anos/F40-900.png"
+                alt="Ferrai"
+              />
+              Ferrai
+            </li>
+            <li>second</li>
+            <li>third</li>
+            <li>second</li>
+            <li>third</li>
+            <li>second</li>
+            <li>third</li>
+            <li>second</li>
+            <li>third</li>
+            <li>second</li>
+            <li>third</li>
+            <li>second</li>
+            <li>third</li>
+            <li>second</li>
+            <li>third</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://img.acp.pt/ResourcesUser/ImagesAC/Classicos/2022/Mes06/Ferrari-F40-35-anos/F40-900.png"
+                alt="Ferrai"
+              />
+              Ferrai
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://img.acp.pt/ResourcesUser/ImagesAC/Classicos/2022/Mes06/Ferrari-F40-35-anos/F40-900.png"
+                alt="Ferrai"
+              />
+              Ferrai
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://img.acp.pt/ResourcesUser/ImagesAC/Classicos/2022/Mes06/Ferrari-F40-35-anos/F40-900.png"
+                alt="Ferrai"
+              />
+              Ferrai
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://img.acp.pt/ResourcesUser/ImagesAC/Classicos/2022/Mes06/Ferrari-F40-35-anos/F40-900.png"
+                alt="Ferrai"
+              />
+              Ferrai
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://img.acp.pt/ResourcesUser/ImagesAC/Classicos/2022/Mes06/Ferrari-F40-35-anos/F40-900.png"
+                alt="Ferrai"
+              />
+              Ferrai
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://img.acp.pt/ResourcesUser/ImagesAC/Classicos/2022/Mes06/Ferrari-F40-35-anos/F40-900.png"
+                alt="Ferrai"
+              />
+              Ferrai
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
